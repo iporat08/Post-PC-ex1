84915cf7b560db266f59c7fba251186ffda8b450
iporat08 <51414337+iporat08@users.noreply.github.com>
2020-04-05 12:26:15 +0300

    added a screenshot

--- app/src/main/java/com/idoporat/ex1/MainActivity.java
+++ app/src/main/java/com/idoporat/ex1/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         textView = (TextView)findViewById(R.id.textView);
         inputEditor = (EditText)findViewById(R.id.textInput);
         Button createButton = (Button)findViewById(R.id.createButton);
