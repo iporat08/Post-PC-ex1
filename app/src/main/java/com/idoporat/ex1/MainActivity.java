18c7b6ead62323de31e299e83a262502be19d914
iporat08 <51414337+iporat08@users.noreply.github.com>
2020-04-07 20:32:42 +0300

    first version with Snackbar message

--- app/src/main/java/com/idoporat/ex1/MainActivity.java
+++ app/src/main/java/com/idoporat/ex1/MainActivity.java
@@ -9,45 +9,46 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
-    static String inputMessage = " ";
-    static String textViewMessage = "Type below and press CREATE!";
-    private TextView textView;
+    static String inputMessage;
     private EditText inputEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView)findViewById(R.id.textView);
+        final Button createButton = (Button)findViewById(R.id.createButton);
+        inputMessage = getString(R.string.initial_input_string);
         inputEditor = (EditText)findViewById(R.id.textInput);
-        Button createButton = (Button)findViewById(R.id.createButton);
-        textView.setText(textViewMessage);
         inputEditor.setText(inputMessage);
         createButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                textView.setText(inputEditor.getText().toString());
-                textViewMessage = inputEditor.getText().toString();
-                inputEditor.setText(getString(R.string.initial_input_string));
-                inputMessage = getString(R.string.initial_input_string);
+                if(inputEditor.getText().toString().equals("")){
+                    int duration =  Snackbar.LENGTH_SHORT;
+                    String empty_toodo_message = getString(R.string.empt_toodo_message);
+                    Snackbar.make(createButton, empty_toodo_message, duration).show();
+                }
+                else{
+                    inputEditor.setText(getString(R.string.initial_input_string));
+                    inputMessage = getString(R.string.initial_input_string);
+                }
             }
         });
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
-
-        outState.putString("textView", textViewMessage);
         outState.putString("inputMessage", inputMessage);
-
         super.onSaveInstanceState(outState);
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        savedInstanceState.getString("textView");
         savedInstanceState.getString("inputMessage");
     }
 }
